52bf16b1a0423d65d4075ad38a8845a5384c9953
TwistedFir3 <flevolan@gmail.com>
2025-09-26 10:23:58 +0200

    Begin python oefeningen
    een begin aan python oefeningen van spl en een toevoeging aan de cheatsheet

---
+++ Python/hobbies.py
@@ -0,0 +1,12 @@
+print("Hello world!")
+
+print("Noem 3 hobbies")
+
+Hobbie1 = input("Wat is je eerste hobbie? ")
+Hobbie2 = input("Wat is je tweede hobbie? ")
+Hobbie3 = input("Wat is je derder hobbie? ")
+
+hobbieLijst = [{Hobbie1}, {Hobbie2}, {Hobbie3}]
+
+print("dit zijn jou hobbies: ")
+print(hobbieLijst)

f1cc4e014a7adebf65415b7542cd11950c03eed2
TwistedFir3 <flevolan@gmail.com>
2025-10-03 09:47:17 +0200

    Opdracht 8 gemaakt

--- Python/SPLopdrachten.py
+++ Python/SPLopdrachten.py
@@ -68,12 +68,23 @@ Zacharia = 1
 
 MijnPresident = input("Ik kies voor: ")
 
-def presidentsverkiezing():
-    while MijnPresident != "UITSLAG":
-        print(MijnPresident)
-        if MijnPresident == "Dominique":
-            Dominique + 1
-        elif MijnPresident == "Zacharia":
-            Zacharia + 1
 
-    
+while MijnPresident != "UITSLAG":
+    if MijnPresident == "Dominique":
+        Dominique += 1
+        MijnPresident = input("Ik kies voor: ")
+    elif MijnPresident == "Zacharia":
+        Zacharia += 1
+        MijnPresident = input("Ik kies voor: ")
+
+def Uitslag():
+    if (Dominique > Zacharia):
+        print("Dominique is tot president verkozen met", Dominique, "stemmen!")
+    elif (Zacharia > Dominique):
+        print("Zacharia is tot president verkozen met", Zacharia, "stemmen!")
+    else:
+        print("Er komt een herverkiezing!")
+
+Uitslag()
+
+# opdracht 9 
